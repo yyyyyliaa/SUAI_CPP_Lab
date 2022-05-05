5d652b015f04454b7eb981500bc4d72f5dca141e
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2022-05-06 00:17:04 +0300

    movingBites() start

---
+++ LAB/L11/D11/pallocDOP11.cpp
@@ -0,0 +1,88 @@
+#include "palloc.h"
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+char memory[16*MemSize]={0};
+unsigned int bitArray[MemSize] = {0};
+unsigned int bitArraySize[MemSize] = {0};
+
+char* palloc(int n){
+    char* arr = NULL;
+    int blocks = 0;
+    if (n % paragraf == 0)
+        n = n / paragraf;
+    else {
+        n = n / paragraf;
+        n++;
+    }
+    blocks = n;
+    int countZero = 0;
+    int indexStart = 0;
+    int flag = 0;
+    for (int i = 0; i< MemSize; i++){
+        if (bitArray[i]==0) countZero++;
+        else countZero = 0;
+        if (countZero ==blocks){
+            indexStart = i-countZero+1;
+            flag = 1;
+            break;
+        }
+    }
+    if (flag==0)
+        return NULL;
+
+
+    for(int i = indexStart; i<indexStart+blocks;i++){
+        bitArray[i] = 1;
+    }
+
+    bitArraySize[indexStart] = blocks;
+
+    return memory+(blocks*paragraf);
+
+}
+
+void pfree(char *p){
+    int start = ((int)(p-memory))/(sizeof(int)*4);
+    for(int i = start; i<start+bitArraySize[start]; i++){
+        bitArray[i] = 0;
+    }
+
+}
+
+void printBitArray() {
+    for (int i = 0; i < MemSize; i++) 
+        cout<<bitArray[i]<< " ";
+    cout<<endl<<endl;
+}
+
+void movingBites(int from, int size, int into){
+    int end = from + size - 1;
+    int blockToMove = into/paragraf;
+
+    int blocks;
+    if ((size%paragraf)==0) blocks = size/paragraf;
+    else blocks = (size/paragraf) + 1;
+
+    int flag = 0;
+
+    for (int i = blockToMove; i<blocks; i++){
+        if(bitArray[i]==1) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag==1) cout<<"Moving is not possible";
+    else {
+        for(int i = into; i<blocks; i++){
+            bitArray[i] = 1;
+        }
+        for (int i = from/paragraf; i<(from/paragraf)+blocks;i++){
+            bitArray[i] = 0;
+        }
+    }
+
+    
+}
---
+++ LAB/L11/D11/pallocDOP11.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define MemSize 100
+#define paragraf 16
+
+char *palloc(int n);
+
+void pfree(char *p);
+
+void printBitArray();
+
+void movingBites(int start, int size);
