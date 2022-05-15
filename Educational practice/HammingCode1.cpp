78c4b60f5553e56c88e19bdb302d742960328aea
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2022-05-16 01:12:53 +0300

    Hamming code finished

---
+++ Educational practice/HammingCode1.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+#define N 5
+#define D 3
+
+unsigned char count_ones(unsigned char n) {
+  unsigned char w = 0;
+  while (n > 0) {
+    w += n & 1;
+    n = n >> 1;
+  }
+  return w;
+}
+
+int main() {
+  int M = 0; 
+  int size = pow(2,N);
+  int mark[32] = {0};
+  int CodeBook[32] = {0};
+  int zero;
+
+  do{
+    zero = -1;
+    for(int i = 0; i<size; i++){
+      if (mark[i]==0){
+        zero = i;
+        break;
+      }
+    }
+    if (zero!=-1){
+    //printf("%d",zero);  
+    CodeBook[M] = zero;
+    mark[zero] = 2; // Отметим взятое слово в массиве пометок.
+    M++;
+    // Шаг 2. \отметим все слова в кандатах на расстоянии меньше d от выбранного.
+    for(int i = CodeBook[M]; i< size;i++){
+    if(mark[i] == 0 && (count_ones(i^CodeBook[M-1])<D)){
+    mark[i] = 1;
+    }
+    printf("%d ", mark[i]);
+    }
+    cout<<endl;
+  }
+  }while(zero!=-1);
+   
+   
+  for (int i = 0; i<size; i++){
+    if ((CodeBook[i]!=0)||(i==0)) cout<<CodeBook[i]<<", ";
+  }
+
+
+
+    return 0;
+}
