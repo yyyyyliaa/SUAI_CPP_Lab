d0db5f3a22c88070775b5af481a51cf1fdf9b0af
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2022-11-05 23:41:59 +0300

    terrible reading from a file

--- LAB/2/СourseWork/karp.cpp
+++ LAB/2/СourseWork/karp.cpp
@@ -1,13 +1,29 @@
 #include "engine.hpp"
 #include <string>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 
 int main(){
-    string pattern = "loh";
-    string text = "loh loh loh";
+
+    string pattern = "He said";
+    string text;
+    string tmp;
+
+    ifstream in("text.txt");
+    if (in.is_open()){
+        while (!in.eof()){
+            getline(in, text, '\0');
+            // text+=tmp;
+        }
+        // while(in){
+        //     in>>tmp;
+        //     text+=tmp;
+        // }
+    }
+    in.close();
 
     engine karp;
     // cout<<karp.getHash(pattern)<<endl;
