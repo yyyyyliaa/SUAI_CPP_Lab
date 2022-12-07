7f6dd082396f02ca4fefc6be52352f34e4f1f899
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2022-12-08 01:32:15 +0300

    added input from the command line

--- LAB/2/СourseWork/Main.cpp
+++ LAB/2/СourseWork/Main.cpp
@@ -2,17 +2,32 @@
 
 using namespace std;
 
-
-int main(){
-
-    string pattern;
-    cout<<"Enter search pattern: ";
-    cin>>pattern;
-
+int main(int argc, char const *argv[]){
     string text = fromFileToString("text.txt");
 
     RabinKarpAlgorithm karp(13, 256);
+
+    string pattern = argv[1];
+
+    cout<<pattern;
    
     cout<<karp.searchPattern(text, pattern)<<endl;
-
+    
+    return 0;
 }
+
+
+
+// int main(){
+
+//     string pattern;
+//     cout<<"Enter search pattern: ";
+//     cin>>pattern;
+
+//     string text = fromFileToString("text.txt");
+
+//     RabinKarpAlgorithm karp(13, 256);
+   
+//     cout<<karp.searchPattern(text, pattern)<<endl;
+
+// }
