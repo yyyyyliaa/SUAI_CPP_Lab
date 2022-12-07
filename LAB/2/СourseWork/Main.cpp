55ffcdd2cc12fbd9f44b494107a84c9bdd6fca43
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2022-12-08 01:34:18 +0300

    delete comments

--- LAB/2/СourseWork/Main.cpp
+++ LAB/2/СourseWork/Main.cpp
@@ -8,26 +8,8 @@ int main(int argc, char const *argv[]){
     RabinKarpAlgorithm karp(13, 256);
 
     string pattern = argv[1];
-
-    cout<<pattern;
    
     cout<<karp.searchPattern(text, pattern)<<endl;
     
     return 0;
 }
-
-
-
-// int main(){
-
-//     string pattern;
-//     cout<<"Enter search pattern: ";
-//     cin>>pattern;
-
-//     string text = fromFileToString("text.txt");
-
-//     RabinKarpAlgorithm karp(13, 256);
-   
-//     cout<<karp.searchPattern(text, pattern)<<endl;
-
-// }
